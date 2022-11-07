e092fa10dc591f5043a0e43f446d3fdcaa878d71
Harinder Singh <harmiglany@yahoo.com>
2022-11-06 20:21:03 -0800

    commit tree cb21546a229b5f3812b773be0b51754a506c301d

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+// const h1 = document.querySelector('#victory');
+newHeader.id = "victory";
+newHeader.textContent = "harinder is the champion";
